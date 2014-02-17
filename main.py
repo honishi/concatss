03cab9469997f1fa8fdad21756b25b622a1428bf
Hiroyuki Onishi <onishi.hiroyuki@gmail.com>
2014-02-17 17:06:52 +0900

    Added background concat processing feature.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 
 import os
 import uuid
+import threading
 from flask import Flask, render_template, request, send_file
 import concatss
 
@@ -11,6 +12,8 @@ OUTPUT_DIR = os.path.dirname(os.path.abspath(__file__)) + '/output'
 
 IMAGE_NAME_PREFIX = 'image'
 ACCEPTABLE_IMAGE_COUNT = 100
+
+WORK_IN_PROGRESS_SUFFIX = '.wip'
 
 app = Flask(__name__)
 
@@ -23,37 +26,52 @@ def index():
 @app.route("/concatenated/<concatenated_id>", methods=["GET"])
 def concatenated(concatenated_id):
     concatenated_filename = OUTPUT_DIR + '/' + concatenated_id
+    if os.path.exists(concatenated_filename + WORK_IN_PROGRESS_SUFFIX):
+        return "working... {}".format(metatag_for_redirect(concatenated_id))
+    elif not os.path.exists(concatenated_filename):
+        return "failed."
     return send_file(concatenated_filename, mimetype='image/jpeg')
 
 
 @app.route("/upload", methods=["POST"])
 def upload():
-    if request.method == 'POST':
-        received_filenames = []
-        for idx in range(1, ACCEPTABLE_IMAGE_COUNT):
-            possible_received_file = IMAGE_NAME_PREFIX + '%02d' % idx
-            if request.files.get(possible_received_file):
-                received_file = request.files[possible_received_file]
-                output_filename = INPUT_DIR + '/' + str(uuid.uuid4())
-                received_file.save(output_filename)
-                received_filenames.append(output_filename)
-        if len(received_filenames) < 2:
-            return "not enough images specified."
+    if request.method != 'POST':
+        return "request method not allowed."
 
-        concatenated_id = str(uuid.uuid4())
-        concatenated_filename = OUTPUT_DIR + '/' + concatenated_id
+    received_filenames = []
+    for idx in range(1, ACCEPTABLE_IMAGE_COUNT):
+        possible_received_file = IMAGE_NAME_PREFIX + '%02d' % idx
+        if request.files.get(possible_received_file):
+            received_file = request.files[possible_received_file]
+            output_filename = INPUT_DIR + '/' + str(uuid.uuid4())
+            received_file.save(output_filename)
+            received_filenames.append(output_filename)
 
-        concat = concatss.ConcatScreenShot()
-        concatenated_image = concat.concatenate_images(received_filenames, concatenated_filename)
+    if len(received_filenames) < 2:
+        return "not enough images specified."
 
-        if concatenated_image:
-            concatenated_url = "/concatenated/%s" % concatenated_id
-            response = "<a href=\"%s\">%s</a>" % (concatenated_url, concatenated_url)
-        else:
-            response = "could not concatenate screenshot from uploaded files."
+    concatenated_id = str(uuid.uuid4())
+    t = threading.Thread(target=concat, args=(concatenated_id, received_filenames))
+    t.start()
 
-        return response
-    return "request method not allowed."
+    return "uploaded... {}".format(metatag_for_redirect(concatenated_id))
+
+
+def metatag_for_redirect(concatenated_id):
+    url = "/concatenated/{}".format(concatenated_id)
+    return "<meta http-equiv=\"refresh\" content=\"2;URL={}\">".format(url)
+
+
+def concat(concatenated_id, received_filenames):
+    concatenated_filename = OUTPUT_DIR + '/' + concatenated_id
+
+    # touch
+    open(concatenated_filename + WORK_IN_PROGRESS_SUFFIX, 'a').close()
+
+    concat = concatss.ConcatScreenShot()
+    concatenated_image = concat.concatenate_images(received_filenames, concatenated_filename)
+
+    os.remove(concatenated_filename + WORK_IN_PROGRESS_SUFFIX)
 
 
 if __name__ == "__main__":
